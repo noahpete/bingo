1408f8d61d036255a269d8e2a33330a1bb6c4156
Noah Peters <97405316+noahpete@users.noreply.github.com>
2025-02-09 00:17:33 -0500

    Random ball generation

--- scenes/ball/bingo_ball.gd
+++ scenes/ball/bingo_ball.gd
@@ -1,2 +1,10 @@
 extends MeshInstance3D
 class_name BingoBall
+
+@onready var color_rect: ColorRect = $SubViewport/CanvasLayer/ColorRect
+@onready var rich_text_label: RichTextLabel = $SubViewport/CanvasLayer/RichTextLabel
+
+
+func _ready() -> void:
+	color_rect.color = Color(randf(), randf(), randf())
+	rich_text_label.text = str(randi_range(1, 99))
--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,2 +1,6 @@
 extends Node
 class_name Main
+
+
+func _ready() -> void:
+	$Entities/BingoMachine/AnimationPlayer.play("dispense")
