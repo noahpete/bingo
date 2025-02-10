246041602705a44e80df020f84a849f65cf7872d
Noah Peters <97405316+noahpete@users.noreply.github.com>
2025-02-09 23:05:03 -0500

    Bingo balls in spinning cage

--- scenes/ball/bingo_ball.gd
+++ scenes/ball/bingo_ball.gd
@@ -1,2 +1,9 @@
 extends RigidBody3D
 class_name BingoBall
+
+
+func _process(delta: float) -> void:
+	pass
+
+func set_velocity(vector: Vector3) -> void:
+	linear_velocity = vector
--- scenes/bingo_machine/bingo_machine.gd
+++ scenes/bingo_machine/bingo_machine.gd
@@ -1,7 +1,7 @@
 extends Node3D
 class_name BingoMachine
 
-const SHAKE_AMOUNT: float = 0.04
+const SHAKE_AMOUNT: float = 0.01
 const SHAKE_FADE: float = 5.0
 
 @export var ball_scene: PackedScene
@@ -10,16 +10,20 @@ const SHAKE_FADE: float = 5.0
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var visuals: Node3D = $Visuals
 @onready var balls: Node3D = $Balls
+@onready var cage_area_3d: Area3D = $Visuals/Cage/Area3D
 
 var original_position: Vector3
 var original_rotation: Vector3
 var shake_strength := 0.0
 var shake_fade := false
 
+var i := 0
+
 
 func _ready() -> void:
 	original_position = visuals.position
 	original_rotation = visuals.rotation
+	cage_area_3d.body_exited.connect(_on_cage_area_exited)
 	_spawn_balls()
 	
 	
@@ -33,6 +37,13 @@ func _process(delta: float) -> void:
 		visuals.position = original_position
 		visuals.rotation = original_rotation
 		shake_fade = false
+		
+
+func _on_cage_area_exited(body) -> void:
+	if body.is_in_group("balls"):
+		i += 1
+		body.queue_free()
+		_spawn_ball()
 	
 	
 func play_animation(animation_name: String) -> void:
@@ -48,6 +59,11 @@ func fade_out_shake() -> void:
 	shake_fade = true
 	
 	
+func add_random_velocity_to_balls() -> void:
+	for ball in balls.get_children():
+		ball.set_velocity(random_offset(1.0))
+	
+	
 func random_offset(amount: float) -> Vector3:
 	return Vector3(
 		randf_range(-amount, amount),
@@ -58,6 +74,10 @@ func random_offset(amount: float) -> Vector3:
 
 func _spawn_balls():
 	for i in range(75):
-		var ball_instance = ball_scene.instantiate()
-		balls.add_child(ball_instance)
-		ball_instance.global_transform.origin = balls.global_transform.origin + random_offset(0.02)
+		_spawn_ball()
+		
+		
+func _spawn_ball() -> void:
+	var ball_instance = ball_scene.instantiate()
+	balls.add_child(ball_instance)
+	ball_instance.global_transform.origin = balls.global_transform.origin + random_offset(0.02)
