7f15f3abb7af81e1cea76db5c29b36172984e84e
Noah Peters <97405316+noahpete@users.noreply.github.com>
2025-02-21 00:47:23 -0500

    Added cage spinning

--- scenes/machine/machine.gd
+++ scenes/machine/machine.gd
@@ -1,2 +1,13 @@
 class_name Machine
 extends Node3D
+
+@onready var cage: Node3D = $Cage
+
+func spin_cage() -> void:
+	var tween = create_tween()
+	tween.tween_property(
+		cage, 
+		"rotation:y", 
+		deg_to_rad(1080), 
+		2.0
+	).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
