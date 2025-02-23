5b35ff3e0be94074c6bc9d37b093fb52c7b31ef5
Noah Peters <97405316+noahpete@users.noreply.github.com>
2025-02-22 19:25:01 -0500

    Create ball and ball mesh

--- scenes/ball/ball_mesh.gd
+++ scenes/ball/ball_mesh.gd
@@ -1,7 +1,5 @@
-class_name Ball
-extends RigidBody3D
-
-@export var game_manager: GameManager
+class_name BallMesh
+extends MeshInstance3D
 
 @onready var color_rect: ColorRect = %ColorRect
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
@@ -14,10 +12,12 @@ var colors = [
 	"#e5ceb4"
 ]
 
+var value: int
+
 
 func _ready() -> void:
 	color_rect.color = Color(colors.pick_random())
-	var value = game_manager.draw_ball()
+	value = GameManager.draw_ball()
 	rich_text_label.text = str(value)
 	if value < 10:
-		rich_text_label.position.x += 3
+		rich_text_label.position.x += 0
